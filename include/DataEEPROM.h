bdd5b351a6dbb9a36f323ee428e5dfc6343b7014
Izimirt <andreu77705@gmail.com>
2026-02-22 19:22:14 +0300

    1.0.1

--- include/DataEEPROM.h
+++ include/DataEEPROM.h
@@ -1,7 +1,23 @@
+
 #pragma once
 
 #include <Arduino.h>
 #include <EEPROM.h>
+
+    //                       |---------------------------------------> EEPROM memory
+    // markerOffset = 0----->|marker|errNum|{ErrStruct}|{ValStruct}|...
+    //             errNumOffset = 1 |      |           |           |
+    //                       |----->|      |           |           |
+    //                       |             |           |           |
+    //                       |errOffset    |           |           |
+    //                       |------------>|           |           |
+    //                       |                         |           |
+    //                       |                         |           |
+    //                       |       valOffset         |           |
+    //                       |------------------------>|           |
+    //                       |                                     |
+    //                       |              fullOffset             |
+    //                       |------------------------------------>|
 
 class DataEEPROM
 {
@@ -13,22 +29,35 @@ class DataEEPROM
 
         static DataEEPROM* Instance(size_t size = 4096);
 
+
+
+        inline uint16_t GetFullOffset();
+
+
+
         void SetError(uint16_t error);
 
         void CleanErrors();
 
         uint16_t* GetErrors();
 
-        uint16_t GetNumErrors();
+        uint8_t GetNumErrors();
+
+        
 
         #ifdef ESP32
-            void SaveChangeValue(uint8_t num, void* ptr, uint8_t type);
 
-            uint8_t GetNumSavedValues();
+            void SaveChangeValue(uint8_t amount, uint8_t* numArr, void** ptrArr, uint8_t* typeArr);
 
-            void* GetSavedValue(uint8_t num);
+            void GetSavedValues(uint8_t** infoArrPtr, uint8_t** valArrPtr);
+
+            void DeleteChangeValue(uint8_t amount, uint8_t* ptr);
+
+            void ChangeStartGet(uint8_t amount, uint8_t* ptr, bool en);
+
+            inline uint8_t GetMaxChangeValAmount();
+
         #endif
-       
 
     private:
 
@@ -36,23 +65,33 @@ class DataEEPROM
 
         void CycleWrite(uint16_t error);
 
-        static const uint8_t maxErrorNum = 50;
-        static const uint8_t maxChangedValNum = 30;
+        inline uint16_t GetMarkerOffset();
+
+        inline uint16_t GetErrNumOffset();
+
+        inline uint16_t GetErrorOffset();
+
+        inline uint16_t GetValOffset();
+
+        static const uint8_t maxErrorNum = 30;
+        static const uint8_t maxChangedValNum = 50;
         static const uint8_t validMarker = 123;
 
-        struct MyStructDataEEPROM
+        uint8_t errNum = 0;
+
+        typedef struct 
         {
-            uint8_t marker;
-
-            uint8_t errNum = 0;
             uint8_t cycleErrNum = 0;
-            uint16_t error[maxErrorNum] = {0};
+            uint16_t error[maxErrorNum] {};
+        } err_struct_t;
+        err_struct_t ErrStruct;
 
-            uint8_t changedValNumLenght = 0;
-            uint8_t changedValByte[maxChangedValNum * 4] = {0};
-            uint8_t changedValNum[maxChangedValNum] = {0};
-        };
-        MyStructDataEEPROM StructDataEEPROM;
+        typedef struct 
+        {
+            uint8_t changedValByte[maxChangedValNum * 4] {};    // (array number * 4) = AppLink vector number
+            uint8_t changedValInfo[maxChangedValNum] {};        // array number = AppLink vector number. [0] bit = have val, [1] bit = get val on start
+        } val_struct_t;
+        val_struct_t ValStruct;
 
         static DataEEPROM* instance;
 };
--- src/DataEEPROM.cpp
+++ src/DataEEPROM.cpp
@@ -7,25 +7,38 @@ DataEEPROM::DataEEPROM(size_t size)
     #ifdef ESP32
         EEPROM.begin(size);
     #endif
-    delay(100);
-    EEPROM.get(0,StructDataEEPROM);
-    if(StructDataEEPROM.marker != validMarker)
+    delay(10);
+    uint8_t marker = 0;
+
+    EEPROM.get(GetMarkerOffset(),marker);
+
+    if(marker != validMarker)
     {
-        StructDataEEPROM.marker = validMarker;
-        StructDataEEPROM.errNum = 0;
-        StructDataEEPROM.cycleErrNum = 0;
-        StructDataEEPROM.changedValNumLenght = 0;
+        EEPROM.get(GetErrNumOffset(),errNum);
+        EEPROM.get(GetErrorOffset(),ErrStruct);
+        EEPROM.get(GetValOffset(),ValStruct);
+
+        marker = validMarker;
+        errNum = 0;
+
+        ErrStruct.cycleErrNum = 0;
         for(int i = 0; i < maxErrorNum; i++)
-            StructDataEEPROM.error[i] = 0;
-        for(int i = 0; i < maxChangedValNum * 4; i++)
-            StructDataEEPROM.changedValByte[i] = 0;
+            ErrStruct.error[i] = 0;
+
+        for(int i = 0; i < (maxChangedValNum * 4); i++)
+            ValStruct.changedValByte[i] = 0;
         for(int i = 0; i < maxChangedValNum; i++)
-            StructDataEEPROM.changedValNum[i] = 0;
-        EEPROM.put(0,StructDataEEPROM);
+            ValStruct.changedValInfo[i] = 0b00000010;
+
+        EEPROM.put(GetMarkerOffset(),marker);
+        EEPROM.put(GetErrNumOffset(),errNum);
+        EEPROM.put(GetErrorOffset(),ErrStruct);
+        EEPROM.put(GetValOffset(),ValStruct);
+
         #ifdef ESP32
             EEPROM.commit();
         #endif
-        delay(100);
+        delay(10);
     }
 }
 
@@ -38,10 +51,15 @@ DataEEPROM *DataEEPROM::Instance(size_t size)
 
 void DataEEPROM::SetError(uint16_t error)
 {
-    EEPROM.get(0,StructDataEEPROM);
+    EEPROM.get(GetErrNumOffset(),errNum);
+    EEPROM.get(GetErrorOffset(),ErrStruct);
+
     CycleWrite(error);
-    EEPROM.put(0,StructDataEEPROM);
-    delay(100);
+
+    EEPROM.put(GetErrNumOffset(),errNum);
+    EEPROM.put(GetErrorOffset(),ErrStruct);
+
+    delay(10);
     #ifdef ESP32 
         EEPROM.commit();
     #endif
@@ -49,22 +67,52 @@ void DataEEPROM::SetError(uint16_t error)
 
 void DataEEPROM::CycleWrite(uint16_t error)
 {
-    StructDataEEPROM.error[StructDataEEPROM.cycleErrNum] = error;
-    StructDataEEPROM.error[(StructDataEEPROM.cycleErrNum + 1) % maxErrorNum] = 0;
-    StructDataEEPROM.cycleErrNum = (StructDataEEPROM.cycleErrNum + 1) % maxErrorNum;
-    if(StructDataEEPROM.errNum < maxErrorNum)
-        StructDataEEPROM.errNum++;
+    ErrStruct.error[ErrStruct.cycleErrNum] = error;
+    ErrStruct.error[(ErrStruct.cycleErrNum + 1) % maxErrorNum] = 0;
+    ErrStruct.cycleErrNum = (ErrStruct.cycleErrNum + 1) % maxErrorNum;
+    if(errNum < maxErrorNum)
+        errNum++;
+}
+
+inline uint16_t DataEEPROM::GetMarkerOffset()
+{
+    return 0;
+}
+
+inline uint16_t DataEEPROM::GetErrNumOffset()
+{
+    return (GetMarkerOffset() + 1);
+}
+
+uint16_t DataEEPROM::GetErrorOffset()
+{
+    return (GetErrNumOffset() + 1);
+}
+
+uint16_t DataEEPROM::GetValOffset()
+{
+    return (GetErrorOffset() + (1 + (maxErrorNum * 2)));
+}
+
+uint16_t DataEEPROM::GetFullOffset()
+{
+    return (GetValOffset() + (maxChangedValNum * 5));
 }
 
 void DataEEPROM::CleanErrors()
 {
-    EEPROM.get(0,StructDataEEPROM);
-    StructDataEEPROM.errNum = 0;
-    StructDataEEPROM.cycleErrNum = 0;
+    EEPROM.get(GetErrNumOffset(),errNum);
+    EEPROM.get(GetErrorOffset(),ErrStruct);
+
+    errNum = 0;
+    ErrStruct.cycleErrNum = 0;
     for(int i = 0; i < maxErrorNum; i++)
-        StructDataEEPROM.error[i] = 0;
-    EEPROM.put(0,StructDataEEPROM);
-    delay(100);
+        ErrStruct.error[i] = 0;
+
+    EEPROM.put(GetErrNumOffset(),errNum);
+    EEPROM.put(GetErrorOffset(),ErrStruct);
+
+    delay(10);
     #ifdef ESP32
         EEPROM.commit();
     #endif
@@ -72,63 +120,115 @@ void DataEEPROM::CleanErrors()
 
 uint16_t* DataEEPROM::GetErrors()
 {
-    EEPROM.get(0,StructDataEEPROM);    
-    return StructDataEEPROM.error;
+    EEPROM.get(GetErrorOffset(),ErrStruct);
+    return ErrStruct.error;
 }
 
-uint16_t DataEEPROM::GetNumErrors()
+uint8_t DataEEPROM::GetNumErrors()
 {
-    EEPROM.get(0,StructDataEEPROM); 
-    return StructDataEEPROM.errNum;
+    EEPROM.get(GetErrNumOffset(),errNum);
+    return errNum;
 }
 
 #ifdef ESP32
 
-    void DataEEPROM::SaveChangeValue(uint8_t num, void* ptr, uint8_t type)
+    void DataEEPROM::SaveChangeValue(uint8_t amount, uint8_t* numArr, void** ptrArr, uint8_t* typeArr)
     {
-        if(num > maxChangedValNum)
-            return;
-        if(ptr == nullptr)
-            return;
+        EEPROM.get(GetValOffset(),ValStruct);
 
-        EEPROM.get(0,StructDataEEPROM);
+        for (int i = 0; i < amount; i++)
+        {
+            uint8_t num = numArr[i];
+            void* ptr = ptrArr[i];
+            uint8_t type = typeArr[i];
 
-        if(type == 1)
-            *(int8_t*)(StructDataEEPROM.changedValByte + (StructDataEEPROM.changedValNumLenght*4)) = *(int8_t*)ptr;
-        else if(type == 2)
-            *(uint8_t*)(StructDataEEPROM.changedValByte + (StructDataEEPROM.changedValNumLenght*4)) = *(uint8_t*)ptr;
-        else if(type == 3)
-            *(int16_t*)(StructDataEEPROM.changedValByte + (StructDataEEPROM.changedValNumLenght*4)) = *(int16_t*)ptr;
-        else if(type == 4)
-            *(uint16_t*)(StructDataEEPROM.changedValByte + (StructDataEEPROM.changedValNumLenght*4)) = *(uint16_t*)ptr;
-        else if(type == 5)
-            *(int32_t*)(StructDataEEPROM.changedValByte + (StructDataEEPROM.changedValNumLenght*4)) = *(int32_t*)ptr;
-        else if(type == 6)
-            *(uint32_t*)(StructDataEEPROM.changedValByte + (StructDataEEPROM.changedValNumLenght*4)) = *(uint32_t*)ptr;
-        else if(type == 7)
-            *(float*)(StructDataEEPROM.changedValByte + (StructDataEEPROM.changedValNumLenght*4)) = *(float*)ptr;
+            if(num > maxChangedValNum)
+                continue;
+            if(ptr == nullptr)
+                continue;
 
-        StructDataEEPROM.changedValNum[StructDataEEPROM.changedValNumLenght] = num;
-        StructDataEEPROM.changedValNumLenght++;
+            ValStruct.changedValInfo[num] |= 0b00000001;
 
-        EEPROM.put(0,StructDataEEPROM);
-        delay(100);
-            EEPROM.commit();
+            *(uint32_t*)(ValStruct.changedValByte + (num * 4)) = 0;    // for cleaning junk
+
+            if(type == 1)
+                *(int8_t*)(ValStruct.changedValByte + (num * 4)) = *(int8_t*)ptr;
+            else if(type == 2)
+                *(uint8_t*)(ValStruct.changedValByte + (num * 4)) = *(uint8_t*)ptr;
+            else if(type == 3)
+                *(int16_t*)(ValStruct.changedValByte + (num * 4)) = *(int16_t*)ptr;
+            else if(type == 4)
+                *(uint16_t*)(ValStruct.changedValByte + (num * 4)) = *(uint16_t*)ptr;
+            else if(type == 5)
+                *(int32_t*)(ValStruct.changedValByte + (num * 4)) = *(int32_t*)ptr;
+            else if(type == 6)
+                *(uint32_t*)(ValStruct.changedValByte + (num * 4)) = *(uint32_t*)ptr;
+            else if(type == 7)
+                *(float*)(ValStruct.changedValByte + (num * 4)) = *(float*)ptr;
+        }
+
+        EEPROM.put(GetValOffset(),ValStruct);
+
+        delay(10);
+            
+        EEPROM.commit();
     }
 
-    uint8_t DataEEPROM::GetNumSavedValues()
+    void DataEEPROM::GetSavedValues(uint8_t** infoArrPtr, uint8_t** valArrPtr)
     {
-        EEPROM.get(0,StructDataEEPROM);
-        return StructDataEEPROM.changedValNumLenght;
+        EEPROM.get(GetValOffset(),ValStruct);
+
+        *infoArrPtr = ValStruct.changedValInfo;
+        *valArrPtr = ValStruct.changedValByte;
     }
 
-    void* DataEEPROM::GetSavedValue(uint8_t num)
+    void DataEEPROM::DeleteChangeValue(uint8_t amount, uint8_t* ptr)
     {
-        if(num > maxChangedValNum)
-            return nullptr;
+        EEPROM.get(GetValOffset(),ValStruct);
 
-        EEPROM.get(0,StructDataEEPROM);
-        return StructDataEEPROM.changedValByte + (num*4);
+        for (int i = 0; i < amount; i++)
+        {
+            uint8_t num = ptr[i];
+            if(num > maxChangedValNum)
+                continue;
+
+            ValStruct.changedValInfo[num] &= (~0b00000001);
+            *(uint32_t*)(ValStruct.changedValByte + (num * 4)) = 0;    // for cleaning junk
+        }
+
+        EEPROM.put(GetValOffset(),ValStruct);
+
+        delay(10);
+            
+        EEPROM.commit();
+    }
+
+    void DataEEPROM::ChangeStartGet(uint8_t amount, uint8_t* ptr, bool en)
+    {
+        EEPROM.get(GetValOffset(),ValStruct);
+
+        for (int i = 0; i < amount; i++)
+        {
+            uint8_t num = ptr[i];
+            if(num > maxChangedValNum)
+                continue;
+
+            if (en)
+                ValStruct.changedValInfo[num] |= 0b00000010;
+            else
+                ValStruct.changedValInfo[num] &= (~0b00000010);
+        }
+
+        EEPROM.put(GetValOffset(),ValStruct);
+
+        delay(10);
+            
+        EEPROM.commit();
+    }
+
+    inline uint8_t DataEEPROM::GetMaxChangeValAmount()
+    {
+        return maxChangedValNum;
     }
 
 #endif
